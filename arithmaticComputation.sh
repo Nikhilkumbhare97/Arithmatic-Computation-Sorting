ca37d340b6a782b442f93a88bc80e3ece965f99d
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-11 23:04:09 +0530

    Added: Store The Result In A Dictionary For Every Computation

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,17 +1,25 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Enter the Three Inputs"
 read a b c
 echo "Three inputs are : "$a $b $c
 
+declare -A resultArray
 result1=$(( $a + $b * $c ))
 echo "Result for the first computation : "$result1
+resultArray[comp1]=$result1
 
 result2=$(( $a * $b + $c ))
 echo "Result for the second computation : "$result2
+resultArray[comp2]=$result2
 
-result3=`expr $c+$a/$b  | bc -l`
+result3=`expr $c+$a/$b | bc -l`
 echo "Result for the third computation : "$result3
+resultArray[comp3]=$result3
 
 result4=$(( $a % $b + $c ))
 echo "Result for the fourth computation : "$result4
+resultArray[comp4]=$result4
+
+echo "${!resultArray[@]}"
+echo "${resultArray[@]}"
